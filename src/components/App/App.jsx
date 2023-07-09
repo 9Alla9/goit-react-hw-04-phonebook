63ceba3a69772e53753e2831a968ec8f347e442d
9Alla9 <konfetcka@gmail.com>
2023-07-09 15:39:33 +0300

    хуки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,80 +1,69 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import css from './App.module.css';
 import { nanoid } from 'nanoid';
 import ContactForm from 'components/ContactForm';
 import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+const DefaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+const App = () => {
+  const [contacts, setContacts] = useState(DefaultContacts);
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      setContacts(JSON.parse(contacts));
     }
-  };
-   componentDidUpdate(prevProps, prevState ) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    };
-  };
-  addName = ({ name, number }) => {
-    if (
-      this.state.contacts.find(
-        contact =>
-          contact.name.toLowerCase() === name.toLowerCase() ||
-          contact.number === number
-      )
-    ) {
-      return alert(`${name} or ${number} is contacts`);
+  }, []);
+  useEffect(() => {
+    if (contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-    this.setState(prevState => {
-      return {
-        contacts: [{ name, number, id: nanoid() }, ...prevState.contacts],
-      };
-    });
+  }, [contacts]);
+
+  const addName = ({ name, number }) => {
+    const names = contacts.map(contact => contact.name);
+    if (names.indexOf(name) >= 0) {
+      alert(name + ' is already in contacts');
+      return;
+    }
+    setContacts(prevContacts => [
+      { name, number, id: nanoid() },
+      ...prevContacts,
+    ]);
   };
-  removeName = id => {
-    this.setState(prevState => ({
+
+  const removeName = id => {
+    setContacts(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
-  handleFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+
+  const handleFilter = e => {
+    setFilter(e.currentTarget.value);
   };
-  getVisibleContacts = () => {
-    let { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
-  render() {
-    const { filter } = this.state;
-    return (
-      <div className={css.container}>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addName} />
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addName} />
 
-        <h2>Contacts</h2>
-        <Filter onChange={this.handleFilter} value={filter} />
-        <ContactList
-          contacts={this.getVisibleContacts()}
-          onRemove={this.removeName}
-        />
-      </div>
-    );
-  }
-}
+      <h2>Contacts</h2>
+      <Filter onChange={handleFilter} value={filter} />
+      <ContactList contacts={getVisibleContacts()} onRemove={removeName} />
+    </div>
+  );
+};
 
 export default App;
